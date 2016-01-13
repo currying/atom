2b69e0b18bce905a3b512daba5144bb74ee0ec02
currying <currying@currying-PC>
2016-01-13 17:40:47 +0800

    Signed-off-by: currying <currying@currying-PC>

--- src/main/webapp/resources/js/post.js
+++ src/main/webapp/resources/js/post.js
@@ -2,11 +2,15 @@ function post() {
 	$.ajax({
 		url : "/atom/rest/login",
 		type : 'POST',
-		data : JSON.stringify({"userId":"admin1","password": "admin"}),
-		contentType: 'application/json; charset=utf-8',
+		data : JSON.stringify({
+			"userId" : "admin",
+			"password" : "admin"
+		}),
+		contentType : 'application/json; charset=utf-8',
 		dataType : 'json',
 		success : function(data, status, xhr) {
-			location.href = "index";
+			if (status == "success")
+				location.href = "index";
 		},
 		Error : function(xhr, error, exception) {
 			alert(exception.toString());
