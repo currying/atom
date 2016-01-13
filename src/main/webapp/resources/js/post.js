9427025e9747b71715b4456229d7468f0175881a
currying <currying@currying-PC>
2016-01-13 17:32:00 +0800

    Signed-off-by: currying <currying@currying-PC>

--- src/main/webapp/resources/js/post.js
+++ src/main/webapp/resources/js/post.js
@@ -2,10 +2,11 @@ function post() {
 	$.ajax({
 		url : "/atom/rest/login",
 		type : 'POST',
-		data : JSON.stringify({"userId":"admin","password": "admin"}),
+		data : JSON.stringify({"userId":"admin1","password": "admin"}),
 		contentType: 'application/json; charset=utf-8',
 		dataType : 'json',
 		success : function(data, status, xhr) {
+			location.href = "index";
 		},
 		Error : function(xhr, error, exception) {
 			alert(exception.toString());
